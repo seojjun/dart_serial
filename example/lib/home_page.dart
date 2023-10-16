f9388b7751ed90bbba593159a26c80e8e7e5611c
jjun <109662579+seojjun@users.noreply.github.com>
2023-10-16 18:38:15 +0900

    test2

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -16,35 +16,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  SerialPort? _port;
-  final _received = <Uint8List>[];
+  SerialPort? _port1;
+  SerialPort? _port2;
+  final _received1 = <Uint8List>[];
+  final _received2 = <Uint8List>[];
 
   final _controller1 = TextEditingController();
-  final sendMsgNode = FocusNode();
+  final _controller2 = TextEditingController();
+  final sendMsgNode1 = FocusNode();
+  final sendMsgNode2 = FocusNode();
   final FocusNode _focusNode = FocusNode();
-  final ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController1 = ScrollController();
+  final ScrollController _scrollController2 = ScrollController();
   bool _ctrlPressed = false;
   String binaryString = '\x03';
 
-  Future<void> _openPort() async {
-    await _port?.close();
+  Future<void> _openPort1() async {
+    await _port1?.close();
 
     final port = await window.navigator.serial.requestPort();
     await port.open(baudRate: 9600);
 
-    _port = port;
+    _port1 = port;
 
-    _startReceiving(port);
+    _startReceiving1(port);
 
     setState(() {});
   }
 
-  Future<void> _writeToPort(Uint8List data) async {
+  Future<void> _openPort2() async {
+    await _port2?.close();
+
+    final port = await window.navigator.serial.requestPort();
+    await port.open(baudRate: 9600);
+
+    _port2 = port;
+
+    _startReceiving2(port);
+
+    setState(() {});
+  }
+
+  Future<void> _writeToPort(Uint8List data, int portNum) async {
     if (data.isEmpty) {
       return;
     }
 
-    final port = _port;
+    final port = portNum == 1 ? _port1 : _port2;
 
     if (port == null) {
       return;
@@ -58,8 +76,8 @@ class _HomePageState extends State<HomePage> {
     await writer.close();
   }
 
-  Future<void> _stopCommand() async {
-    final port = _port;
+  Future<void> _stopCommand(int portNum) async {
+    final port = portNum == 1 ? _port1 : _port2;
 
     if (port == null) {
       return;
@@ -73,18 +91,18 @@ class _HomePageState extends State<HomePage> {
     await writer.close();
   }
 
-  Future<void> _startReceiving(SerialPort port) async {
+  Future<void> _startReceiving1(SerialPort port) async {
     final reader = port.readable.reader;
 
     while (true) {
       final result = await reader.read();
-      _received.add(result.value);
+      _received1.add(result.value);
 
-      if (_scrollController.hasClients) {
+      if (_scrollController1.hasClients) {
         WidgetsBinding.instance.addPostFrameCallback(
           (timeStamp) {
-            _scrollController.animateTo(
-              _scrollController.position.maxScrollExtent,
+            _scrollController1.animateTo(
+              _scrollController1.position.maxScrollExtent,
               duration: Duration(milliseconds: 100),
               curve: Curves.easeOut,
             );
@@ -96,11 +114,37 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  void _sendEvent() {
-    var text = '${_controller1.text}\r';
-    _writeToPort(Uint8List.fromList(text.codeUnits));
-    _controller1.clear();
-    FocusScope.of(context).requestFocus(sendMsgNode);
+  Future<void> _startReceiving2(SerialPort port) async {
+    final reader = port.readable.reader;
+
+    while (true) {
+      final result = await reader.read();
+      _received2.add(result.value);
+
+      if (_scrollController2.hasClients) {
+        WidgetsBinding.instance.addPostFrameCallback(
+          (timeStamp) {
+            _scrollController2.animateTo(
+              _scrollController2.position.maxScrollExtent,
+              duration: Duration(milliseconds: 100),
+              curve: Curves.easeOut,
+            );
+          },
+        );
+      }
+
+      setState(() {});
+    }
+  }
+
+  void _sendEvent(int portNum) {
+    var text =
+        portNum == 1 ? '${_controller1.text}\r' : '${_controller2.text}\r';
+    _writeToPort(Uint8List.fromList(text.codeUnits), portNum);
+    portNum == 1 ? _controller1.clear() : _controller2.clear();
+    portNum == 1
+        ? FocusScope.of(context).requestFocus(sendMsgNode1)
+        : FocusScope.of(context).requestFocus(sendMsgNode2);
   }
 
   @override
@@ -112,16 +156,33 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Serial Port'),
         actions: [
           IconButton(
-            onPressed: _openPort,
+            onPressed: _openPort1,
             icon: Icon(Icons.device_hub),
             tooltip: 'Open Serial Port',
           ),
           IconButton(
-            onPressed: _port == null
+            onPressed: _port1 == null
                 ? null
                 : () async {
-                    await _port?.close();
-                    _port = null;
+                    await _port1?.close();
+                    _port1 = null;
+
+                    setState(() {});
+                  },
+            icon: Icon(Icons.close),
+            tooltip: 'Close Serial Port',
+          ),
+          IconButton(
+            onPressed: _openPort2,
+            icon: Icon(Icons.device_hub),
+            tooltip: 'Open Serial Port',
+          ),
+          IconButton(
+            onPressed: _port2 == null
+                ? null
+                : () async {
+                    await _port2?.close();
+                    _port2 = null;
 
                     setState(() {});
                   },
@@ -133,13 +194,22 @@ class _HomePageState extends State<HomePage> {
       body: RawKeyboardListener(
         focusNode: _focusNode,
         onKey: (RawKeyEvent event) {
+          int portNum = 1;
+          if (sendMsgNode1.hasFocus) {
+            portNum = 1;
+          } else if (sendMsgNode2.hasFocus) {
+            portNum = 2;
+          }
+
           if (event is RawKeyDownEvent) {
             if (event.logicalKey == LogicalKeyboardKey.controlLeft ||
                 event.logicalKey == LogicalKeyboardKey.controlRight) {
               _ctrlPressed = true;
             } else if (_ctrlPressed &&
                 event.logicalKey == LogicalKeyboardKey.keyC) {
-              _stopCommand();
+              _stopCommand(portNum);
+              print('copy');
+              print(portNum);
               _ctrlPressed = false;
             }
           } else if (event is RawKeyUpEvent) {
@@ -149,61 +219,131 @@ class _HomePageState extends State<HomePage> {
             }
           }
         },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Row(
           children: [
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white54),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: _received.isNotEmpty
-                      ? ListView(
-                          controller: _scrollController,
-                          padding: const EdgeInsets.all(4),
-                          children: _received.map((e) {
-                            final text = String.fromCharCodes(e);
-                            return Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Text(text),
-                            );
-                          }).toList(),
-                        )
-                      : Center(
-                          child: Text(
-                            'No data received yet.',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: TextField(
-                      controller: _controller1,
-                      focusNode: sendMsgNode,
-                      onSubmitted: (String value) {
-                        _sendEvent();
-                      },
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white54),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: _received1.isNotEmpty
+                            ? ListView(
+                                controller: _scrollController1,
+                                padding: const EdgeInsets.all(4),
+                                children: _received1.map((e) {
+                                  final text = String.fromCharCodes(e);
+                                  return Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(text),
+                                  );
+                                }).toList(),
+                              )
+                            : Center(
+                                child: Text(
+                                  'No data received yet.',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                      ),
                     ),
                   ),
-                  Gap(8),
                   Padding(
                     padding: const EdgeInsets.all(4.0),
-                    child: ElevatedButton(
-                      child: const Text('Send'),
-                      onPressed: () {
-                        _sendEvent();
-                      },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: TextField(
+                            controller: _controller1,
+                            focusNode: sendMsgNode1,
+                            onSubmitted: (String value) {
+                              _sendEvent(1);
+                            },
+                          ),
+                        ),
+                        Gap(8),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ElevatedButton(
+                            child: const Text('Send'),
+                            onPressed: () {
+                              _sendEvent(1);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white54),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: _received2.isNotEmpty
+                            ? ListView(
+                                controller: _scrollController2,
+                                padding: const EdgeInsets.all(4),
+                                children: _received2.map((e) {
+                                  final text = String.fromCharCodes(e);
+                                  return Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text(text),
+                                  );
+                                }).toList(),
+                              )
+                            : Center(
+                                child: Text(
+                                  'No data received yet.',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: TextField(
+                            controller: _controller2,
+                            focusNode: sendMsgNode2,
+                            onSubmitted: (String value) {
+                              _sendEvent(2);
+                            },
+                          ),
+                        ),
+                        Gap(8),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ElevatedButton(
+                            child: const Text('Send'),
+                            onPressed: () {
+                              _sendEvent(2);
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
